7a46de350934b359a90e603b3f39056ea235c49f
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-01-04 17:43:30 +0900

    d3d12 memory allocators added

--- src/d3d12/d3d12_header_common.h
+++ src/d3d12/d3d12_header_common.h
@@ -9,6 +9,7 @@ using DxgiAdapter = IDXGIAdapter4;
 using DxgiSwapchain = IDXGISwapChain4;
 using D3d12Device = ID3D12Device10;
 using D3d12CommandQueue = ID3D12CommandQueue;
+using D3d12Fence = ID3D12Fence1;
 using D3d12CommandList = ID3D12GraphicsCommandList7;
 }
 #endif
