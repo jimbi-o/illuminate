f45079a742b2e5ba46357a10b533ab2e0d308f03
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-01-08 16:22:14 +0900

    hash map added

---
+++ include/illuminate/util/hash_map.h
@@ -0,0 +1,34 @@
+#ifndef ILLUMINATE_UTIL_HASH_MAP_H
+#define ILLUMINATE_UTIL_HASH_MAP_H
+#include "illuminate/core/strid.h"
+namespace illuminate {
+template <typename T, typename A>
+class HashMap {
+ public:
+  static const uint32_t kDefaultTableSize = 128;
+  HashMap(A* allocator, const uint32_t table_size = kDefaultTableSize)
+      : allocator_(allocator)
+      , table_size_(table_size)
+      , table_(AllocateArray<T*>(allocator_, table_size_))
+  {
+    for (uint32_t i = 0; i < table_size_; i++) {
+      table_[i] = nullptr;
+    }
+  }
+  virtual ~HashMap() {}
+  const T& Get(const StrHash key) const { return *table_[key % table_size_]; }
+  T& Get(const StrHash key) { return *table_[key % table_size_]; }
+  bool Insert(const StrHash key, T&& val) {
+    auto index = key % table_size_;
+    if (table_[index] != nullptr) return false;
+    table_[index] = Allocate<T>(allocator_);
+    (*table_[index]) = std::move(val);
+    return true;
+  }
+ private:
+  A* allocator_;
+  uint32_t table_size_;
+  T** table_;
+};
+}
+#endif
---
+++ src/util/hash_map.cpp
@@ -0,0 +1,34 @@
+#include "illuminate/memory/memory_allocation.h"
+#include "illuminate/util/hash_map.h"
+#include "doctest/doctest.h"
+TEST_CASE("hash map") {
+  using namespace illuminate;
+  const uint32_t buffer_size = 128;
+  std::byte buffer[buffer_size]{};
+  LinearAllocator allocator(buffer, buffer_size);
+  const uint32_t table_size = 10;
+  struct TestStruct {
+    uint32_t a{0}, b{0};
+  };
+  HashMap<TestStruct, LinearAllocator> map(&allocator, table_size);
+  auto key = CalcStrHash("key0");
+  auto key2 = CalcStrHash("key2");
+  CHECK_UNARY(map.Insert(key, TestStruct{3, 5}));
+  CHECK_UNARY(map.Insert(key2, TestStruct{8, 1}));
+  CHECK_EQ(map.Get(key).a, 3);
+  CHECK_EQ(map.Get(key).b, 5);
+  CHECK_EQ(map.Get(key2).a, 8);
+  CHECK_EQ(map.Get(key2).b, 1);
+  allocator.Reset();
+  HashMap<void*, LinearAllocator> map2(&allocator, table_size);
+  auto ptr = Allocate<uint32_t>(&allocator);
+  *ptr = 10;
+  auto ptr2 = Allocate<uint32_t>(&allocator);
+  *ptr2 = 101;
+  CHECK_EQ(*ptr, 10);
+  CHECK_EQ(*ptr2, 101);
+  CHECK_UNARY(map2.Insert(key, ptr));
+  CHECK_UNARY(map2.Insert(key2, ptr2));
+  CHECK_EQ(*reinterpret_cast<uint32_t*>(map2.Get(key)), 10);
+  CHECK_EQ(*reinterpret_cast<uint32_t*>(map2.Get(key2)), 101);
+}
