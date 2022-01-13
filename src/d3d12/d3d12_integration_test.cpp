dd08ed5e9dc95bc9c2a5392b75703a09640f5b3d
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-01-13 12:36:46 +0900

    CreateUnorderedView caller implemented

--- src/d3d12/d3d12_integration_test.cpp
+++ src/d3d12/d3d12_integration_test.cpp
@@ -88,32 +88,114 @@ class DescriptorCpu {
   uint32_t handle_increment_size_[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES]{};
   uint64_t heap_start_[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES]{};
 };
-bool CreateView(const ViewType view_type, const BufferConfig& config, ID3D12Resource* resource, const D3D12_CPU_DESCRIPTOR_HANDLE* handle) {
-  // TODO
+auto GetUavDimention(const D3D12_RESOURCE_DIMENSION dimension, const uint16_t depth_or_array_size) {
+  switch (dimension) {
+    case D3D12_RESOURCE_DIMENSION_UNKNOWN: {
+      assert(false && "D3D12_RESOURCE_DIMENSION_UNKNOWN selected");
+      return D3D12_UAV_DIMENSION_UNKNOWN;
+    }
+    case D3D12_RESOURCE_DIMENSION_BUFFER: {
+      return D3D12_UAV_DIMENSION_BUFFER;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE1D: {
+      if (depth_or_array_size > 1) {
+        return D3D12_UAV_DIMENSION_TEXTURE1DARRAY;
+      }
+      return D3D12_UAV_DIMENSION_TEXTURE1D;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE2D: {
+      if (depth_or_array_size > 1) {
+        return D3D12_UAV_DIMENSION_TEXTURE2DARRAY;
+      }
+      return D3D12_UAV_DIMENSION_TEXTURE2D;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE3D: {
+      return D3D12_UAV_DIMENSION_TEXTURE3D;
+    }
+  }
+  logerror("GetUavDimention invalid dimension {}", dimension);
+  assert(false && "invalid dimension");
+  return D3D12_UAV_DIMENSION_UNKNOWN;
+}
+bool CreateView(D3d12Device* device, const ViewType view_type, const BufferConfig& config, ID3D12Resource* resource, const D3D12_CPU_DESCRIPTOR_HANDLE* handle) {
   switch (view_type) {
     case ViewType::kCbv: {
-      assert(false);
-      return true;
+      assert(false && "CreateView cbv not implemented");
+      return false;
     }
     case ViewType::kSrv: {
-      assert(false);
-      return true;
+      assert(false && "CreateView srv not implemented");
+      return false;
     }
     case ViewType::kUav: {
-      assert(false);
-      return true;
+      auto desc = D3D12_UNORDERED_ACCESS_VIEW_DESC{
+        .Format = config.format,
+        .ViewDimension = GetUavDimention(config.dimension, config.depth_or_array_size),
+      };
+      switch (desc.ViewDimension) {
+        case D3D12_UAV_DIMENSION_UNKNOWN: {
+          assert(false && "invalid uav view dimension");
+          break;
+        }
+        case D3D12_UAV_DIMENSION_BUFFER: {
+          assert(false && "D3D12_UAV_DIMENSION_BUFFER not implemented");
+          desc.Buffer = {
+          };
+          break;
+        }
+        case D3D12_UAV_DIMENSION_TEXTURE1D: {
+          desc.Texture1D = {
+            .MipSlice = 0,
+          };
+          break;
+        }
+        case D3D12_UAV_DIMENSION_TEXTURE1DARRAY: {
+          desc.Texture1DArray = {
+            .MipSlice = 0,
+            .FirstArraySlice = 0,
+            .ArraySize = config.depth_or_array_size,
+          };
+          break;
+        }
+        case D3D12_UAV_DIMENSION_TEXTURE2D: {
+          desc.Texture2D = {
+            .MipSlice = 0,
+            .PlaneSlice = 0,
+          };
+          break;
+        }
+        case D3D12_UAV_DIMENSION_TEXTURE2DARRAY: {
+          desc.Texture2DArray = {
+            .MipSlice = 0,
+            .FirstArraySlice = 0,
+            .ArraySize = config.depth_or_array_size,
+            .PlaneSlice = 0,
+          };
+          break;
+        }
+        case D3D12_UAV_DIMENSION_TEXTURE3D: {
+          desc.Texture3D = {
+            .MipSlice = 0,
+            .FirstWSlice = 0,
+            .WSize = config.depth_or_array_size,
+          };
+          break;
+        }
+      }
+      device->CreateUnorderedAccessView(resource, nullptr, &desc, *handle);
+      return false;
     }
     case ViewType::kSampler: {
-      assert(false);
+      assert(false && "CreateView sampler not implemented");
       return false;
     }
     case ViewType::kRtv: {
-      assert(false);
-      return true;
+      assert(false && "CreateView rtv not implemented");
+      return false;
     }
     case ViewType::kDsv: {
-      assert(false);
-      return true;
+      assert(false && "CreateView dsv not implemented");
+      return false;
     }
   }
   assert(false && "CreateView invalid view type");
@@ -179,13 +261,13 @@ auto GetTestJson() {
       "sample_count": 1,
       "sample_quality": 0,
       "layout": "unknown",
-      "flags": ["rtv"],
+      "flags": ["uav"],
       "mip_width": 0,
       "mip_height": 0,
       "mip_depth": 0,
-      "initial_state": "rtv",
+      "initial_state": "uav",
       "clear_color": [1,0,0,1],
-      "descriptor_type": ["rtv", "srv"]
+      "descriptor_type": ["uav", "srv"]
     }
   ],
   "render_pass": [
@@ -251,7 +333,7 @@ auto GetTestJson() {
     "srv": 1,
     "uav": 1,
     "sampler": 0,
-    "rtv": 1,
+    "rtv": 0,
     "dsv": 0
   }
 }
@@ -374,8 +456,9 @@ auto CreateBuffer(const BufferConfig& config, D3D12MA::Allocator* allocator) {
       .Depth = config.mip_depth,
     },
   };
-  BufferAllocation buffer_allocation;
-  auto hr = allocator->CreateResource2(&allocation_desc, &resource_desc, config.initial_state, &config.clear_value, nullptr, &buffer_allocation.allocation, IID_PPV_ARGS(&buffer_allocation.resource));
+  auto clear_value = (resource_desc.Dimension != D3D12_RESOURCE_DIMENSION_BUFFER && (resource_desc.Flags & (D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET | D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL)) != 0) ? &config.clear_value : nullptr;
+  BufferAllocation buffer_allocation{};
+  auto hr = allocator->CreateResource2(&allocation_desc, &resource_desc, config.initial_state, clear_value, nullptr, &buffer_allocation.allocation, IID_PPV_ARGS(&buffer_allocation.resource));
   if (FAILED(hr)) {
     logerror("failed to create resource. {}", hr);
     assert(false && "failed to create resource");
@@ -436,7 +519,7 @@ TEST_CASE("d3d12 integration test") { // NOLINT
       CAPTURE(j);
       auto cpu_handle = descriptor_cpu.GetHandle(render_graph.buffer_list[i].name, render_graph.buffer_list[i].descriptor_type[j]);
       CHECK_NE(cpu_handle, nullptr);
-      CHECK_UNARY(CreateView(render_graph.buffer_list[i].descriptor_type[j], render_graph.buffer_list[i], buffer.resource, cpu_handle));
+      CHECK_UNARY(CreateView(device.Get(), render_graph.buffer_list[i].descriptor_type[j], render_graph.buffer_list[i], buffer.resource, cpu_handle));
     }
   }
   Swapchain swapchain;
