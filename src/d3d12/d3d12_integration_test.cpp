6aab1c9281ad355b7e74f7b3bcbad69af59e9522
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-01-14 13:18:10 +0900

    CreateShaderResourceView caller implemented

--- src/d3d12/d3d12_integration_test.cpp
+++ src/d3d12/d3d12_integration_test.cpp
@@ -88,7 +88,7 @@ class DescriptorCpu {
   uint32_t handle_increment_size_[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES]{};
   uint64_t heap_start_[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES]{};
 };
-auto GetUavDimention(const D3D12_RESOURCE_DIMENSION dimension, const uint16_t depth_or_array_size) {
+auto GetUavDimension(const D3D12_RESOURCE_DIMENSION dimension, const uint16_t depth_or_array_size) {
   switch (dimension) {
     case D3D12_RESOURCE_DIMENSION_UNKNOWN: {
       assert(false && "D3D12_RESOURCE_DIMENSION_UNKNOWN selected");
@@ -113,9 +113,39 @@ auto GetUavDimention(const D3D12_RESOURCE_DIMENSION dimension, const uint16_t de
       return D3D12_UAV_DIMENSION_TEXTURE3D;
     }
   }
-  logerror("GetUavDimention invalid dimension {}", dimension);
+  logerror("GetUavDimension invalid dimension {}", dimension);
   assert(false && "invalid dimension");
   return D3D12_UAV_DIMENSION_UNKNOWN;
+}
+auto GetSrvDimension(const D3D12_RESOURCE_DIMENSION dimension, const uint16_t depth_or_array_size) {
+  switch (dimension) {
+    case D3D12_RESOURCE_DIMENSION_UNKNOWN: {
+      assert(false && "D3D12_RESOURCE_DIMENSION_UNKNOWN selected");
+      return D3D12_SRV_DIMENSION_UNKNOWN;
+    }
+    case D3D12_RESOURCE_DIMENSION_BUFFER: {
+      return D3D12_SRV_DIMENSION_BUFFER;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE1D: {
+      if (depth_or_array_size > 1) {
+        return D3D12_SRV_DIMENSION_TEXTURE1DARRAY;
+      }
+      return D3D12_SRV_DIMENSION_TEXTURE1D;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE2D: {
+      if (depth_or_array_size > 1) {
+        return D3D12_SRV_DIMENSION_TEXTURE2DARRAY;
+      }
+      return D3D12_SRV_DIMENSION_TEXTURE2D;
+    }
+    case D3D12_RESOURCE_DIMENSION_TEXTURE3D: {
+      return D3D12_SRV_DIMENSION_TEXTURE3D;
+    }
+  }
+  // cubemap?
+  logerror("GetSrvDimension invalid dimension {}", dimension);
+  assert(false && "invalid dimension");
+  return D3D12_SRV_DIMENSION_UNKNOWN;
 }
 bool CreateView(D3d12Device* device, const ViewType view_type, const BufferConfig& config, ID3D12Resource* resource, const D3D12_CPU_DESCRIPTOR_HANDLE* handle) {
   switch (view_type) {
@@ -124,13 +154,106 @@ bool CreateView(D3d12Device* device, const ViewType view_type, const BufferConfi
       return false;
     }
     case ViewType::kSrv: {
-      assert(false && "CreateView srv not implemented");
-      return false;
+      auto desc = D3D12_SHADER_RESOURCE_VIEW_DESC{
+        .Format = config.format,
+        .ViewDimension = GetSrvDimension(config.dimension, config.depth_or_array_size),
+        .Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING,
+      };
+      switch (desc.ViewDimension) {
+        case D3D12_SRV_DIMENSION_UNKNOWN: {
+          assert(false && "invalid srv view dimension");
+          break;
+        }
+        case D3D12_SRV_DIMENSION_BUFFER: {
+          assert(false && "D3D12_SRV_DIMENSION_BUFFER not implemented");
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE1D: {
+          desc.Texture1D = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE1DARRAY: {
+          desc.Texture1DArray = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .FirstArraySlice = 0,
+            .ArraySize = config.depth_or_array_size,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE2D: {
+          desc.Texture2D = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .PlaneSlice = 0,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE2DARRAY: {
+          desc.Texture2DArray = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .FirstArraySlice = 0,
+            .ArraySize = config.depth_or_array_size,
+            .PlaneSlice = 0,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE2DMS: {
+          desc.Texture2DMS = {};
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE2DMSARRAY: {
+          desc.Texture2DMSArray = {};
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURE3D: {
+          desc.Texture3D = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURECUBE: {
+          desc.TextureCube = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_TEXTURECUBEARRAY: {
+          desc.TextureCubeArray = {
+            .MostDetailedMip = 0,
+            .MipLevels = ~0U,
+            .First2DArrayFace = 0,
+            .NumCubes = config.depth_or_array_size,
+            .ResourceMinLODClamp = 0.0f,
+          };
+          break;
+        }
+        case D3D12_SRV_DIMENSION_RAYTRACING_ACCELERATION_STRUCTURE: {
+          desc.RaytracingAccelerationStructure = {
+            .Location = resource->GetGPUVirtualAddress(),
+          };
+          break;
+        }
+      }
+      device->CreateShaderResourceView(resource, &desc, *handle);
+      return true;
     }
     case ViewType::kUav: {
       auto desc = D3D12_UNORDERED_ACCESS_VIEW_DESC{
         .Format = config.format,
-        .ViewDimension = GetUavDimention(config.dimension, config.depth_or_array_size),
+        .ViewDimension = GetUavDimension(config.dimension, config.depth_or_array_size),
       };
       switch (desc.ViewDimension) {
         case D3D12_UAV_DIMENSION_UNKNOWN: {
@@ -183,7 +306,7 @@ bool CreateView(D3d12Device* device, const ViewType view_type, const BufferConfi
         }
       }
       device->CreateUnorderedAccessView(resource, nullptr, &desc, *handle);
-      return false;
+      return true;
     }
     case ViewType::kSampler: {
       assert(false && "CreateView sampler not implemented");
