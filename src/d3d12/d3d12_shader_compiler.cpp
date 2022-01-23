c3602cd2325365c1ffd7aadc81ea71d91dab4990
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-01-23 10:32:26 +0900

    use dxcompiler.dll instad of dxcompiler.lib

--- src/d3d12/d3d12_shader_compiler.cpp
+++ src/d3d12/d3d12_shader_compiler.cpp
@@ -1,39 +1,48 @@
 #include "d3d12_shader_compiler.h"
 #include "d3d12_src_common.h"
+#include "../win32_dll_util_macro.h"
 namespace illuminate {
 // https://simoncoenen.com/blog/programming/graphics/DxcCompiling
-IDxcUtils* CreateDxcUtils() {
+HMODULE LoadDxcLibrary() {
+  auto library = LoadLibrary("dxcompiler.dll");
+  assert(library && "failed to load dxcompiler.dll");
+  return library;
+}
+IDxcCompiler3* CreateDxcShaderCompiler(HMODULE library) {
+  IDxcCompiler3* compiler = nullptr;
+  auto hr = CALL_DLL_FUNCTION(library, DxcCreateInstance)(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler));
+  if (SUCCEEDED(hr)) { return compiler; }
+  logerror("CreateDxcShaderCompiler failed. {}", hr);
+  return nullptr;
+}
+IDxcUtils* CreateDxcUtils(HMODULE library) {
   IDxcUtils* utils = nullptr;
-  auto hr = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&utils));
-  if (SUCCEEDED(hr)) return utils;
+  auto hr = CALL_DLL_FUNCTION(library, DxcCreateInstance)(CLSID_DxcUtils, IID_PPV_ARGS(&utils));
+  if (SUCCEEDED(hr)) { return utils; }
   logerror("CreateDxcUtils failed. {}", hr);
   return nullptr;
 }
 IDxcIncludeHandler* CreateDxcIncludeHandler(IDxcUtils* utils) {
   IDxcIncludeHandler* include_handler = nullptr;
   auto hr = utils->CreateDefaultIncludeHandler(&include_handler);
-  if (SUCCEEDED(hr)) return include_handler;
+  if (SUCCEEDED(hr)) { return include_handler; }
   logerror("CreateDefaultIncludeHandler failed. {}", hr);
-  return nullptr;
-}
-IDxcCompiler3* CreateDxcShaderCompiler() {
-  IDxcCompiler3* compiler = nullptr;
-  auto hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler));
-  if (SUCCEEDED(hr)) return compiler;
-  logerror("CreateDxcShaderCompiler failed. {}", hr);
   return nullptr;
 }
 }
 #include "doctest/doctest.h"
 TEST_CASE("compile shader") { // NOLINT
   using namespace illuminate; // NOLINT
-  auto utils = CreateDxcUtils();
+  auto library = LoadDxcLibrary();
+  CHECK_NE(library, nullptr);
+  auto compiler = CreateDxcShaderCompiler(library);
+  CHECK_NE(compiler, nullptr); // NOLINT
+  auto utils = CreateDxcUtils(library);
   CHECK_NE(utils, nullptr); // NOLINT
   auto include_handler = CreateDxcIncludeHandler(utils);
   CHECK_NE(include_handler, nullptr); // NOLINT
-  auto compiler = CreateDxcShaderCompiler();
-  CHECK_NE(compiler, nullptr); // NOLINT
-  CHECK_EQ(compiler->Release(), 0);
   CHECK_EQ(include_handler->Release(), 0);
   CHECK_EQ(utils->Release(), 0);
+  CHECK_EQ(compiler->Release(), 0);
+  CHECK_UNARY(FreeLibrary(library));
 }
