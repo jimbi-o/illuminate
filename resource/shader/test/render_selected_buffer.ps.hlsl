3d19c6e548bfbb33b5f0adf19b3a192183bb6812
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-02-21 17:51:00 +0900

    shader rootsig flag changed

--- resource/shader/test/render_selected_buffer.ps.hlsl
+++ resource/shader/test/render_selected_buffer.ps.hlsl
@@ -5,7 +5,7 @@ SamplerState tex_sampler : register(s0);
 #define CopyFullscreenRootsig " \
 RootConstants(num32BitConstants=1, b0, visibility=SHADER_VISIBILITY_PIXEL), \
 DescriptorTable(SRV(t0, numDescriptors=unbounded,                       \
-                    flags = DESCRIPTORS_VOLATILE),                      \
+                    flags = DATA_STATIC_WHILE_SET_AT_EXECUTE),          \
                 visibility=SHADER_VISIBILITY_PIXEL),                    \
 DescriptorTable(Sampler(s0), visibility=SHADER_VISIBILITY_PIXEL)        \
 "
