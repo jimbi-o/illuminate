23202340b1b08a821d2e9a30a73ef777fc26290b
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-04-25 09:19:22 +0900

    (cont.) shader compiler refactor in progress

--- resource/shader/test/prez.ps.hlsl
+++ resource/shader/test/prez.ps.hlsl
@@ -1,5 +1,5 @@
 #include "shader/test/prez.hlsli"
 [RootSignature(PrezRootsig)]
-float4 main(PrezVsOutput input) : SV_TARGET0 {
-  return float4(1.0f, 0.0f, 1.0f, 1.0f);
+[earlydepthstencil]
+void main(PrezVsOutput input) {
 }
