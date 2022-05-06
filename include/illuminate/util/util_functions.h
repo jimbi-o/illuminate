6dd4a883466628f659d5fa577fc4a1e207e03351
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-05-07 00:20:51 +0900

    GetDirectory

--- include/illuminate/util/util_functions.h
+++ include/illuminate/util/util_functions.h
@@ -1,7 +1,9 @@
 #ifndef ILLUMINATE_UTIL_UTIL_FUNCTIONS_H
 #define ILLUMINATE_UTIL_UTIL_FUNCTIONS_H
+#include <filesystem>
 namespace illuminate {
 bool SucceedMultipleIndex(const uint32_t array_size, const uint32_t* array_size_array, uint32_t* array);
 void SetArrayValues(const uint32_t val, const uint32_t array_size, uint32_t* array);
+std::filesystem::path GetDirectory(const char* const path);
 }
 #endif
